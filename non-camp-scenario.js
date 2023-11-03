ad1524bc6ebc989e86dd9342be6082de047127e4
Sealorent <vikylorentseaputra@gmail.com>
2023-11-03 20:09:28 +0700

    feat(update)

--- non-camp-scenario.js
+++ non-camp-scenario.js
@@ -15,46 +15,36 @@ export default function () {
   
   var token =  Actions.login();
   
-  sleep(1);
 
   Actions.getProfile(token);
 
-  sleep(1);
 
   Actions.dashboard(token);
 
-  sleep(1);
 
   var uid_category = Actions.learningVideoMaster(token);
 
-  sleep(1);
 
   var uid = Actions.learningVideoCategory(token, uid_category);
 
   Actions.learningVideoDetail(token, uid);
 
-  sleep(1);
 
   Actions.classInfo(token);
 
-  sleep(1);
 
   Actions.classCurrent(token);
 
-  sleep(1);
 
   Actions.placementTest(token);
 
-  sleep(1);
 
   Actions.academicReport(token);
 
-  sleep(1);
 
   var uid = Actions.notificationList(token);
 
   Actions.promotionDetails(token, uid);
 
-  sleep(1);
   
 }
