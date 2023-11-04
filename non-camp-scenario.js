58c0cfb96498773ab5fba1c6cd1ce8e6e7ff7718
Sealorent <vikylorentseaputra@gmail.com>
2023-11-04 08:01:36 +0700

    feat(update)!

--- non-camp-scenario.js
+++ non-camp-scenario.js
@@ -2,49 +2,41 @@
 import { sleep } from 'k6';
 import Actions from './actions.js';
 
-export const options = {
+const options = {
   // Key configurations for avg load test in this section
   stages: [
-    { duration: '5m', target: 100 }, // traffic ramp-up from 1 to 100 users over 5 minutes.
-    { duration: '30m', target: 100 }, // stay at 100 users for 30 minutes
-    { duration: '5m', target: 0 }, // ramp-down to 0 users
+    { target: 10 }, // below normal load
+    { target: 100, duration: '1m' }, // ramp-up from 10 to 100 users over 1 minute
+    { target: 1000, duration: '10m' }, // stay at 1000 users for 9 minutes
+    { target: 0, duration: '1m' }, // ramp-down to 0 users over 1 minute
   ],
 };
+
 
 export default function () {
   
   var token =  Actions.login();
-  
 
   Actions.getProfile(token);
 
-
   Actions.dashboard(token);
 
-
   var uid_category = Actions.learningVideoMaster(token);
-
 
   var uid = Actions.learningVideoCategory(token, uid_category);
 
   Actions.learningVideoDetail(token, uid);
 
-
   Actions.classInfo(token);
-
 
   Actions.classCurrent(token);
 
-
   Actions.placementTest(token);
 
-
   Actions.academicReport(token);
-
 
   var uid = Actions.notificationList(token);
 
   Actions.promotionDetails(token, uid);
-
   
 }
