6205204a5646e405f7fce817e378ec61861983a8
Sealorent <vikylorentseaputra@gmail.com>
2024-01-10 08:21:57 +0700

    Refactor code for improved performance and readability

---
+++ nestjs/action.js
@@ -0,0 +1,24 @@
+
+import { sleep } from 'k6';
+import http from 'k6/http';
+
+export default function () {
+
+    const url = `https://belajar-nest.cap.lcpare.com/auth/sign-in`;
+
+    const data = {
+        email: 'dev@languagecenter.id',
+        password: 'officerlc',
+        token_fcm: 'eMmpTeJoSSuW30dGUTnaSF:APA91bEYgfM2JEdlc4XmXJ1-mWv-ID7g08G5bVMHJa6NeJxLDPX-eofvn9tdLA3zX3FG4zaNtA37iIbbsS3Vyvv2PmjngdUsb7JL0mdKcbF6IpjCYdrtUI1cVHjG6oIUppmeoEw8OFwE'
+    }
+
+    const payload = JSON.stringify({ email: data.email, password: data.password, token_fcm: data.token_fcm });
+
+    const headers = { 'Content-Type': 'application/json' };
+
+    const response = http.post(url, payload, { headers });
+
+    console.log(response.body);
+    
+}
+
